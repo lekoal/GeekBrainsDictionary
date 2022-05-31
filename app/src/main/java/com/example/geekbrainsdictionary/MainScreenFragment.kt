f5b08cf392dd02a97e5c09870c9e8184702c2ccc
lekoal <coreglus@gmail.com>
2022-05-31 12:11:09 +0300

    Binding, fragment & views are added

---
+++ app/src/main/java/com/example/geekbrainsdictionary/MainScreenFragment.kt
@@ -0,0 +1,31 @@
+package com.example.geekbrainsdictionary
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.geekbrainsdictionary.databinding.FragmentMainScreenBinding
+
+class MainScreenFragment : Fragment() {
+
+    private var _binding: FragmentMainScreenBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentMainScreenBinding.inflate(inflater)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+
+    companion object {
+        fun newInstance() = MainScreenFragment()
+    }
+}
